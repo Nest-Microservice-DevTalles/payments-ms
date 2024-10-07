46d75ab4844a72eb08abaa1386a070aeab0da352
Magnus <magnus@shipedge.com>
2024-10-06 21:44:17 -0400

    capitulo 12 - 4

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,13 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { envs } from 'src/config';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { envs, NATS_SERVICE } from 'src/config';
 import Stripe from 'stripe';
 import { PaymentSessionDto } from './dto/payment-session.dto';
 import { Request, Response } from 'express';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class PaymentsService {
     private readonly stripe = new Stripe(envs.stripeSecret);
+    private readonly logger = new Logger('PaymentsService');
 
+    constructor(
+        @Inject(NATS_SERVICE) private readonly client: ClientProxy
+    ){}
+    
     async createPaymentSession(paymentSessionDto: PaymentSessionDto){
         const {currency, items, orderId} = paymentSessionDto;
         const lineItems = items.map(item => {
@@ -59,11 +65,13 @@ export class PaymentsService {
         switch (event.type) {
             case 'charge.succeeded':
                 const chargeSucceeded = event.data.object;
-                // TODO llam nuestro microservicio
-                console.log({
-                    metadata: chargeSucceeded.metadata,
-                    orderId: chargeSucceeded.metadata.orderId
-                });
+                const payload = {
+                    stripePaymentsId: chargeSucceeded.id,
+                    orderId: chargeSucceeded.metadata.orderId,
+                    receiptUrl: chargeSucceeded.receipt_url
+                }
+                // this.logger.log({payload});
+                this.client.emit('payment.succeeded', payload);
                 break;
             default:
                 console.log(`Event: ${event.type} not handled`);
